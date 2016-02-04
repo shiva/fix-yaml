d8cf7b239e8e79886e91a805da7fb16de06dbc58
Shiva <shiv@shiv.me>
2016-02-04 00:29:07 -0800

    Prune unwanted tags

--- src/fix-yaml.js
+++ src/fix-yaml.js
@@ -33,18 +33,58 @@ if (typeof String.prototype.endsWith !== 'function') {
     };
 }
 
-function process(data)
-{
-    delete data.layout;
-    delete data.published;
-    delete data.type;
-    delete data.status;
+Date.prototype.toYMDString = function() {
+    return this.getFullYear() +
+        '-' + (this.getMonth() + 1) +
+        '-' + this.getDate();
+};
+
+function getDateFromFileName(fullpath) {
+    var filename = path.parse(fullpath).name;
+    var dateParts = filename.split("-");
+
+    return new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+}
+
+function shouldDelete(data, f) {
+    if (f === 'tags') {
+        return ((data.tags === null) || (data.tags === '\\[\\]'));
+    }
+
+    if (f === 'categories') {
+        return ((data.categories.length == 0) || (data.categories === '\\[\\]'));
+    }
+
+    if (f === 'title') {
+        return false;
+    }
+
+    if (f === 'date') {
+        return false;
+    }
+
+    return true;
+}
+
+function process(data, filename) {
+    for(var f in data) {
+        if (shouldDelete(data, f)) {
+            delete data[f];
+        }
+    }
 
     if (data.description === undefined) {
         data.description = data.title;
     }
+
+    if (data.date === undefined) {
+        date = getDateFromFileName(filename);
+        data.date = date.toYMDString();
+    }
 }
-        //
+
+var filesToFix = [];
+
 argv._.forEach(function(entry) {
     if (entry.endsWith('README.md')) {
         // skip README.md
@@ -57,11 +97,19 @@ argv._.forEach(function(entry) {
         var contents = fs.readFileSync(filename, 'utf8');
 
         data = fm(contents);
-        process(data.attributes);
+        process(data.attributes, entry);
         console.log(data);
 
         //console.log(util.inspect(data, false, 10, true));
     } catch (err) {
         console.log(err.stack || String(err));
+        filesToFix.push(entry);
     }
-})
+});
+
+if (filesToFix.length > 0) {
+    console.log("\n Need to fix: ");
+    filesToFix.forEach(function(entry) {
+        console.log(entry);
+    });
+}
